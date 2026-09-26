6df19ca43ad59a4bb97f916445d46db540a653f7
Fabio Luiz Paranhos Borelli <fabioborelli@msn.com>
2026-09-26 04:12:50 -0300

    URL de produção na nuvem

--- ClientApp/src/environments/environment.ts
+++ ClientApp/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tree-app-axdrhresexaaf6gs.brazilsouth-01.azurewebsites.net' // URL de produção na nuvem
+  apiUrl: 'https://tree-api-ayhxawb8fxacbxgy.westus3-01.azurewebsites.net' // URL de produção na nuvem
 };
